f03fdbd22ca024f0df155f2d6a71825a23fb8149
lucasoliver06 <lucasoliesc@gmail.com>
2026-09-23 23:54:28 +0200

    Test modificado 23/09

--- SecondsCalculatorTest1.java
+++ SecondsCalculatorTest1.java
@@ -9,43 +9,84 @@ class SecondsCalculatorTest1 {
 		
 		// Test h/m/s
 		assertEquals(0, sec.SegonsFins(1, 1, 1980, 0, 0, 0));
-		assertEquals(1, sec.SegonsFins(1, 1, 1980, 0, 0, 1));
-		assertEquals(60, sec.SegonsFins(1, 1, 1980, 0, 1, 0));
-		assertEquals(3600, sec.SegonsFins(1, 1, 1980, 1, 0, 0));
 		
-		// Test dies
-		assertEquals(86400, sec.SegonsFins(2, 1, 1980, 0, 0, 0));
+		assertEquals(86400, sec.SegonsFins(2, 1, 1980, 0, 0, 0)); //Test segundos un dia
 		
 		// Test mesos
-		assertEquals(2678400, sec.SegonsFins(1, 2, 1980, 0, 0, 0)); // Test mes amb 31 dies
 		assertEquals(5184000, sec.SegonsFins(1, 3, 1980, 0, 0, 0)); // Test mes amb 29 dies
-		assertEquals(7862400, sec.SegonsFins(1, 4, 1980, 0, 0, 0)); // Test  mes amb 30 dies
-		assertEquals(36720000, sec.SegonsFins(1, 3, 1981, 0, 0, 0)); // Test  mes amb 30 dies
+		assertEquals(7862400, sec.SegonsFins(1, 4, 1980, 0, 0, 0)); // Test mes amb 30 dies
+		assertEquals(36720000, sec.SegonsFins(1, 3, 1981, 0, 0, 0)); // Test mes amb 30 dies
 		
 		// Test anys
-		assertEquals(31622400, sec.SegonsFins(1, 1, 1981, 0, 0, 0)); //Test any traspas
 		assertEquals(126230400, sec.SegonsFins(1, 1, 1984, 0, 0, 0)); //Test any no traspas
 		assertEquals(157852800, sec.SegonsFins(1, 1, 1985, 0, 0, 0)); //Test dos anys traspas
 		
 		//Tests negativos
-		assertEquals(-1, sec.SegonsFins(-1, 1, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, -1, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, -1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, -1, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, 0, -1, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, 0, 0, -1));
+		assertEquals(-1, sec.SegonsFins(-1, 1, 1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(1, -1, 1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(1, 1, -1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, -1, 0, 0));
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, 0, -1, 0));
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, 0, 0, -1));
 		
-		//Tests fueras de límite
-		assertEquals(-1, sec.SegonsFins(0, 1, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 0, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1979, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(32, 1, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 13, 1981, 0, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, 24, 0, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, 0, 60, 0));
-		assertEquals(-1, sec.SegonsFins(1, 1, 1981, 0, 0, 60));
-		
+		//-----------------------------------------------------------
 
+		//Test límit i frontera segons
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, 0, 0, 60));
+		assertEquals(59, sec.SegonsFins(1, 1, 1980, 0, 0, 59));
+		assertEquals(58, sec.SegonsFins(1, 1, 1980, 0, 0, 58));
+		assertEquals(1, sec.SegonsFins(1, 1, 1980, 0, 0, 1));
+		
+		//Test límit i frontera minuts
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, 0, 60, 0));
+		assertEquals(3540, sec.SegonsFins(1, 1, 1980, 0, 59, 0));
+		assertEquals(3480, sec.SegonsFins(1, 1, 1980, 0, 58, 0));
+		assertEquals(60, sec.SegonsFins(1, 1, 1980, 0, 1, 0));
+
+		//Test límit i frontera hores
+		assertEquals(-1, sec.SegonsFins(1, 1, 1980, 24, 0, 0));
+		assertEquals(82800, sec.SegonsFins(1, 1, 1980, 23, 0, 0));
+		assertEquals(79200, sec.SegonsFins(1, 1, 1980, 22, 0, 0));
+		assertEquals(3600, sec.SegonsFins(1, 1, 1980, 1, 0, 0));
+		
+		//------------------------------------------
+		//Test límit i frontera dies
+		assertEquals(-1, sec.SegonsFins(0, 1, 1980, 0, 0, 0));		
+
+		// Límite superior Enero (31 días)
+		assertEquals(2505600, sec.SegonsFins(30, 1, 1980, 0, 0, 0));
+		assertEquals(2592000, sec.SegonsFins(31, 1, 1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(32, 1, 1980, 0, 0, 0));		
+
+		// Límite superior Abril (30 días)
+		assertEquals(10281600, sec.SegonsFins(29, 4, 1980, 0, 0, 0));
+		assertEquals(10368000, sec.SegonsFins(30, 4, 1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(31, 4, 1980, 0, 0, 0));	
+
+		// Límite superior Febrero bisiesto (29 días) 
+		assertEquals(5011200, sec.SegonsFins(28, 2, 1980, 0, 0, 0));		
+		assertEquals(5097600, sec.SegonsFins(29, 2, 1980, 0, 0, 0));		
+		assertEquals(-1, sec.SegonsFins(30, 2, 1980, 0, 0, 0));	
+
+		// Límite superior Febrero no bisiesto (28 días)
+		assertEquals(36547200, sec.SegonsFins(27, 2, 1981, 0, 0, 0));		
+		assertEquals(36633600, sec.SegonsFins(28, 2, 1981, 0, 0, 0));	
+		assertEquals(-1, sec.SegonsFins(29, 2, 1981, 0, 0, 0));
+		
+		//------------------------------------------
+		
+		//Test límit i frontera mesos
+		assertEquals(-1, sec.SegonsFins(1, 0, 1980, 0, 0, 0));
+		assertEquals(0, sec.SegonsFins(1, 1, 1980, 0, 0, 0));
+		assertEquals(2678400, sec.SegonsFins(1, 2, 1980, 0, 0, 0));
+		assertEquals(26352000, sec.SegonsFins(1, 11, 1980, 0, 0, 0));
+		assertEquals(28944000, sec.SegonsFins(1, 12, 1980, 0, 0, 0));
+		assertEquals(-1, sec.SegonsFins(1, 13, 1980, 0, 0, 0));
+		
+		//Test límit i frontera anys
+		assertEquals(-1, sec.SegonsFins(1, 1, 1979, 0, 0, 0));
+		assertEquals(0, sec.SegonsFins(1, 1, 1980, 0, 0, 0));
+		assertEquals(31622400, sec.SegonsFins(1, 1, 1981, 0, 0, 0));
 		
 	}
 	
